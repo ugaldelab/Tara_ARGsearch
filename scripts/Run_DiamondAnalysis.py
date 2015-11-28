fc0cdeb7f11c5068d1f191a409a958d83558a3cc
Juan Ugalde <juanuu@gmail.com>
2015-11-28 18:34:42 -0300

    Added parsing of results for diamond script

--- scripts/Run_DiamondAnalysis.py
+++ scripts/Run_DiamondAnalysis.py
@@ -143,6 +143,8 @@ for line in open(args.arg_categories, 'r'):
 
     line = line.rstrip()
     gene, function, aro = line.split("\t")
+    gene = gene.rstrip("p01")
+    gene = gene.rstrip("p1")
     arg_categories[gene].append((function, aro))
 
 # Process each file
